e8384b6715463cc19407dba2ea8d64f4c833981d
malitvin <malitvin@ncsu.edu>
2016-05-13 15:35:11 -0400

    Set up movement with rigidbody 2d

--- Assets/Scripts/Gameplay/Player/PlayerMovement.cs
+++ Assets/Scripts/Gameplay/Player/PlayerMovement.cs
@@ -9,37 +9,37 @@ namespace RENEGADES.Gameplay.Player
     public class PlayerMovement : MonoBehaviour
     {
         private Rigidbody2D rigidBody;
-        private Rigidbody2D  RigidBody
+        private Rigidbody2D  PlayerRigidBody
         {
             get { return rigidBody ?? (rigidBody = GetComponent<Rigidbody2D>()); }
         }
 
-        private Vector3 InputDirection;
-
-        private const float AXIS_THRESHOLD = 0.05f;
-
         private float LeftJoyStickX;
         private float LeftJoyStickY;
 
+        private const float MaxSpeed = 3.5f;
+        private float ForceAdd = 35f;
+
+        private const float DRAG = 4.0f;
+
         private void Awake()
         {
-            InputDirection = Vector3.zero;
+            PlayerRigidBody.drag = DRAG;
         }
+
+        private void Update()
+        {
+            LeftJoyStickX = Input.GetAxis(GameInput.GetInput(GameInput.PlayerInput.LeftJoyHorizontal));
+            LeftJoyStickY = -Input.GetAxis(GameInput.GetInput(GameInput.PlayerInput.LeftJoyVertical));
+        }
+
         // Update is called once per frame
         private void FixedUpdate()
         {
-            LeftJoyStickX = Input.GetAxis(GameInput.GetInput(GameInput.PlayerInput.LeftJoyHorizontal));
-            if (Mathf.Abs(LeftJoyStickX) > 0.05f)
-            {
-                InputDirection.x = LeftJoyStickX;
-                transform.position += InputDirection/15;
-            }
-            LeftJoyStickY = Input.GetAxis(GameInput.GetInput(GameInput.PlayerInput.LeftJoyVertical));
-            if (Mathf.Abs(LeftJoyStickY) > 0.05f)
-            {
-                InputDirection.y = -LeftJoyStickY;
-                transform.position += InputDirection/15;
-            }
+
+            PlayerRigidBody.velocity = Vector3.ClampMagnitude(PlayerRigidBody.velocity, MaxSpeed);
+            PlayerRigidBody.AddForce((Vector2.right*LeftJoyStickX)* ForceAdd);
+            PlayerRigidBody.AddForce((Vector2.up * LeftJoyStickY) * ForceAdd);
 
         }
 
