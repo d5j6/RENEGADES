2ad8bb2b981196b4746e5beadd3af9b61fadcdc6
malitvin <malitvin@ncsu.edu>
2016-06-23 13:50:05 -0400

    switched all cameras to ortho and made background sprite stretch script

---
+++ Assets/Scripts/Gameplay/Background/BackgroundStretch.cs
@@ -0,0 +1,38 @@
+﻿//Unity
+using UnityEngine;
+
+namespace RENEGADES.Gameplay.Background
+{
+
+    public class BackgroundStretch : MonoBehaviour
+    {
+        private SpriteRenderer sr;
+        private SpriteRenderer SR
+        {
+            get { return sr ?? (sr = GetComponent<SpriteRenderer>()); }
+        }
+
+        private float worldScreenHeight;
+        private float worldScreenWidth;
+
+        private void Awake()
+        {
+            GetDimensions();
+            ResizeBackground();
+        }
+
+        private void GetDimensions()
+        {
+            worldScreenHeight = GameObject.FindGameObjectWithTag("BackgroundCAM").GetComponent<Camera>().orthographicSize * 2;
+            worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+        }
+
+        private void ResizeBackground()
+        {
+            transform.localScale = new Vector3(
+                worldScreenWidth / SR.sprite.bounds.size.x,
+                worldScreenHeight / SR.sprite.bounds.size.y, 1);
+        }
+        
+    }
+}
